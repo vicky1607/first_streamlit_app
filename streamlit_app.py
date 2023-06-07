428f1a205649d5400ddcdca9695017af2b2848bd
vicky1607 <67050935+vicky1607@users.noreply.github.com>
2023-06-08 04:59:14 +0530

    Update streamlit_app.py
    Added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast')
+streamlit.text('Poha/Idli/Dosa/Omlet/Fruit slad')
+
+
+streamlit.header('Lunch')
+streamlit.text('Chawal / Rajjma/Veggi/Papad/Sweets')
