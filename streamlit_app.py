15d38a8bd1cad6d53fdd75f8dae9dc5803806086
vicky1607 <67050935+vicky1607@users.noreply.github.com>
2023-06-08 16:28:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('Poha/Idli/Dosa/Omlet/Fruit slad')
 
 streamlit.header('Lunch')
 streamlit.text('Chawal / Rajjma/Veggi/Papad/Sweets')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
